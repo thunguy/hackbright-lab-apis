fdc4ee2aa1863e62b67844ad24a7d50ca1cc8b47
Thu Nguyen <thunguy@me.com>
2020-05-16 09:57:44 -0700

    make request to Event Search endpoint to search events and display to user

--- server.py
+++ server.py
@@ -10,7 +10,7 @@ app.secret_key = 'SECRETSECRETSECRET'
 
 # This configuration option makes the Flask interactive debugger
 # more useful (you should remove this line in production though)
-app.config['PRESERVE_CONTEXT_ON_EXCEPTION'] = True
+# app.config['PRESERVE_CONTEXT_ON_EXCEPTION'] = True
 
 
 API_KEY = os.environ['TICKETMASTER_KEY']
@@ -35,28 +35,24 @@ def find_afterparties():
     """Search for afterparties on Eventbrite"""
 
     keyword = request.args.get('keyword', '')
-    postalcode = request.args.get('zipcode', '')
+    zipcode = request.args.get('zipcode', '')
     radius = request.args.get('radius', '')
     unit = request.args.get('unit', '')
     sort = request.args.get('sort', '')
 
     url = 'https://app.ticketmaster.com/discovery/v2/events'
-    payload = {'apikey': API_KEY}
+    payload = {'apikey': API_KEY,
+               'keyword': keyword,
+               'zipcode': zipcode,
+               'radius': radius,
+               'unit': unit,
+               'sort': sort}
 
-    # TODO: Make a request to the Event Search endpoint to search for events
-    #
-    # - Use form data from the user to populate any search parameters
-    #
-    # - Make sure to save the JSON data from the response to the `data`
-    #   variable so that it can display on the page. This is useful for
-    #   debugging purposes!
-    #
-    # - Replace the empty list in `events` with the list of events from your
-    #   search results
+    response = requests.get(url, params=payload)
 
-    data = {'Test': ['This is just some test data'],
-            'page': {'totalElements': 1}}
-    events = []
+    data = response.json()
+    
+    events = data['_embedded']['events']
 
     return render_template('search-results.html',
                            pformat=pformat,
